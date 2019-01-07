096c5cf73c6280703dfd868c64e594958e88ab34
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2019-01-06 19:45:36 -0600

    templatize kube resources

--- kube/apply-resources.sh
+++ kube/apply-resources.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+#clean
+rm resources/jenkins-deployment.yaml
+
+PASSWORD=`uuidgen`
+#generate configuration from templates
+cat templates/jenkins-deployment.yaml.tmpl | sed "s/ADMIN_PASSWORD_VALUE/$PASSWORD/g" > resources/jenkins-deployment.yaml
+
+# create secrets that can't be created via files (I think)
 kubectl create secret generic jenkins-ssh-key --from-file=id_rsa=../ci/jenkins/.ssh/id_rsa
+
+# have k8s create resources
 kubectl create -f resources/
+
+echo "Initial login: 'admin' / '$PASSWORD'"
